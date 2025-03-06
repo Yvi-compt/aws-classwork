0367e978230f8c263fb78845e1a7c96f17dbc973
Yvi-compt <yvesplacide@gmail.com>
2025-03-05 21:08:39 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,9 +6,14 @@ terraform {
     }
   }
 }
-
 provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+/*provider "aws" {
    aws_access_keys.id = var.aws_access_keys
     aws_secret_access_keys.id = var. aws_secret_access_keys
   region = "us-east-1"  # Change region if needed
-}
+}*/
