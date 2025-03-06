f08498ee5299368735f28e89a0d2e07da01a4d22
Yvi-compt <yvesplacide@gmail.com>
2025-03-05 21:06:05 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -4,3 +4,17 @@ type = string
 variable "aws_secret_access_keys" {
       type =string
 }
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  sensitive   = true
+}
