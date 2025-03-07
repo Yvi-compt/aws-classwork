893422885f6d38047e4de97bc3918157f9550ef1
Yves PLACIDE <yvesplacide@gmail.com>
2025-03-07 18:36:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucket_name # Change to a unique name
-  aws_s3_bucket_acl    = "private"  
+  #aws_s3_bucket_acl    = "private"  
 
   tags = {
     Name        = "My_Bucket"
